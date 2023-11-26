3f65f567908905aa51688dc8f7ee10141d2adcea
SebastianStovall <sebastianstovall@gmail.com>
2023-11-26 17:42:37 -0600

    refactored ProjectList and ProjectInput classes to use inheritance from a base class with generics. Set up a State class to handle multiple forms of state for the future

--- src/app.ts
+++ src/app.ts
@@ -37,16 +37,25 @@ class Project {
 
 // APP STATE MANAGEMENT --------------------------------------------------------------------------------------------------------------------------------------------//
 
-type Listener = (items: Project[]) => void // define structure of a listener function (each listener function should take in an array of project objects, and return void)
+type Listener<T> = (items: T[]) => void // define structure of a listener function (each listener function should take in an array of project objects, and return void)
 
-class ProjectState {   // this class will handle global state for our projects
+class State<T> {
+    protected listeners: Listener<T>[] = [] // an array of listener functions
 
-    private listeners: Listener[] = [] // an array of listener functions
+    // when calling projectState.addListener, it is both defining a listener that gets stored in the ProjectState but also updates
+    // assignedProjects variable inside of the ProjectList class to be what the listner function gets invoked with when its called in
+    // addProject with listenerFn(this.projects.slice()). The projects are then rendered with ProjectLists's renderProjects() method
+    addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn)
+    }
+}
+
+class ProjectState extends State<Project> {   // this class will handle global state for our projects
     private projects: Project[] = []  // if this were Redux, this would essentially act as your store, and the methods would be reducer actions
     private static instance: ProjectState
 
     private constructor() {
-
+        super()
     }
 
     // make this a singleton class... we will only ever need one instance for state management since we want a SINGLE SOURCE OF TRUTH
@@ -57,14 +66,6 @@ class ProjectState {   // this class will handle global state for our projects
             this.instance = new ProjectState()
             return this.instance
         }
-    }
-
-
-    // when calling projectState.addListener, it is both defining a listener that gets stored in the ProjectState but also updates
-    // assignedProjects variable inside of the ProjectList class to be what the listner function gets invoked with when its called in
-    // addProject with listenerFn(this.projects.slice()). The projects are then rendered with ProjectLists's renderProjects() method
-    addListener(listenerFn: Listener) {
-        this.listeners.push(listenerFn)
     }
 
     // this method will invoke on successful submission of a new project. It creates the project, then calls out to any listners (like the one in ProjectList class, to display the new list)
@@ -93,27 +94,59 @@ const projectState = ProjectState.getInstance()  // global constant that can be 
 
 
 
+// Component Base Class  <---- refactors ProjectList and ProjectInput to be a single class witht the use of generics and inheritance
+
+// cannot instaniate this class due to abstract keyword
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement
+    hostElement: T  // hostElement and Element are going to be changing HTMLElements, so use a generic here so we can always ensure we receive the correct type
+    element: U
+
+    constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string) {
+        this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement // gives access to the correct <template></template> tag you need to target (must typecast to HTMLTemplateElement so TS knows your grabbing such an element. Otherwise TS just knows its a regular HTML element which might not have the properties its looking for)
+        this.hostElement = document.getElementById(hostElementId)! as T // <--- T plugs in as HTMLElement  // the element where we want the content to be eventually rendered in the HTML
+
+        const importedNode = document.importNode(this.templateElement.content, true) // !!! this is the imported HTML content of the templateElement (all the content rendered inside of <template></template> tag (including the tag itself) )
+        this.element = importedNode.firstElementChild as U // <--- plugs in element as HTMLElement  // this targets the firstElement of the <template></template> tag... which is the <section></section> tag
+        if(newElementId) {
+            this.element.id = newElementId  // apply the CSS depending on which type the list is
+        }
+
+        this.attach(insertAtStart)
+    }
+
+    private attach(insertAtBeginning: boolean) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element);  // !!!!!! INSERT THE <section></section> THAT INCLUDES OUR PROJECT LIST INTO THE <div id="app"></div> SO WE CAN DISPLAY IT !!!!!! //
+    }
+
+    // forces any inheriting classes to use these two methods (and defines their structure)
+    abstract configure(): void
+    abstract renderContent(): void
+
+}
+
+
+
+
+
+
 
 // DISPLAYING ALL PROJECTS (LIST) ----------------------------------------------------------------------------------------------------------------------------------- //
 
-
-class ProjectList {
-    templateElement: HTMLTemplateElement  // the <template> tag we are reaching out to... ( <template/> used to hold client-side content that you don't want to be rendered when a page loads)
-    hostElement: HTMLDivElement // the final output will eventually be rendered in the <div id="app"></div> tag
-    element: HTMLElement // !! a HTMLSectionElement type is not a thing, so we use HTMLElment instead. BUT THIS IS REACHING OUT SPECFICALLY TO A <section></section> tag
+class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[]
 
     constructor(private type: 'active' | 'finished') {  // need to pass in which List you want rendered (either active projects or finished projects). This is used to determine the CSS of the list output
-        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement // gives access to the <template id='project-list'></template> tag (must typecast to HTMLTemplateElement so TS knows your grabbing such an element. Otherwise TS just knows its a regular HTML element which might not have the properties its looking for)
-        this.hostElement = document.getElementById('app')! as HTMLDivElement  // the element where we want the content to be eventually rendered in the HTML
+        super('project-list','app',false,`${type}-projects` )
         this.assignedProjects = []
-
-
-        const importedNode = document.importNode(this.templateElement.content, true) // !!! this is the imported HTML content of the templateElement (all the content rendered inside of <template></template> tag (including the tag itself) )
-        this.element = importedNode.firstElementChild as HTMLElement  // this targets the firstElement of the <template></template> tag... which is the <section></section> tag
         this.element.id = `${this.type}-projects`  // apply the CSS depending on which type the list is
 
 
+        this.configure()
+        this.renderContent()  // insert our content into the DOM once the <section></section> has been attached
+    }
+
+    configure() {
         // hook up the listener so when a project gets created we can render the projects with renderProjects() method
         projectState.addListener((projects: Project[]) => {
 
@@ -125,13 +158,15 @@ class ProjectList {
                     return project.status === ProjectStatus.Finished  // if finished list, only show finished projects
                 }
             })
-
             this.assignedProjects = filteredProjects // This listener is a function that takes an array of projects as its parameter and updates the this.assignedProjects property of the ProjectList instance with this array.
             this.renderProjects()
         })
+    }
 
-        this.attach() // attaches our created element <section>[...]</section> into the dom
-        this.renderContent()  // insert our content into the DOM once the <section></section> has been attached
+    renderContent() {
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('ul')!.id = listId  // grabs the <ul></ul> tag in the <section></section> element and gives it an id so we can target it
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'   // grabs the <h2></h2> tag in the <section></section> element and injects content into the tag
     }
 
     // once the listener gets invoked, we reach out to the list we want it to be rendered, then we create a list item for the project and display it in our list
@@ -144,30 +179,10 @@ class ProjectList {
             listEl.appendChild(listItem)
         }
     }
-
-    private renderContent() {
-        const listId = `${this.type}-projects-list`
-        this.element.querySelector('ul')!.id = listId  // grabs the <ul></ul> tag in the <section></section> element and gives it an id so we can target it
-        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'   // grabs the <h2></h2> tag in the <section></section> element and injects content into the tag
-    }
-
-    private attach() {
-        this.hostElement.insertAdjacentElement('beforeend', this.element);  // !!!!!! INSERT THE <section></section> THAT INCLUDES OUR PROJECT LIST INTO THE <div id="app"></div> SO WE CAN DISPLAY IT !!!!!! //
-    }
-
-    // private addProject() {
-
-    // }
 }
 
 
-
-
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
 
 
 
@@ -233,23 +248,13 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {        /
 
 
 
-class ProjectInput {
-    templateElement: HTMLTemplateElement  // <template> tag... (used to hold client-side content that you don't want to be rendered when a page loads)
-    hostElement: HTMLDivElement // output will eventually be rendered in the <div id="app"></div> tag
-    element: HTMLFormElement // this is the <form></form> rendered in the <template></template>
-    titleInputElement: HTMLInputElement                                                             // input tag on line 16 in index HTML
-    descriptionInputElement: HTMLInputElement                                                       // textarea tag on line 20 in index HTML
-    peopleInputElement: HTMLInputElement                                                            // input tag on line 24 in index HTML
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement              // input tag on line 16 in index HTML
+    descriptionInputElement: HTMLInputElement        // textarea tag on line 20 in index HTML
+    peopleInputElement: HTMLInputElement             // input tag on line 24 in index HTML
 
     constructor() {
-        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement // gives access to the <template id='project-input'></template> tag (must typecast to HTMLTemplateElement so TS knows your grabbing such an element. Otherwise TS just knows its a regular HTML element which might not have the properties its looking for)
-        this.hostElement = document.getElementById('app')! as HTMLDivElement  // the <div id="app"></div> where we want the content to be eventually rendered in the HTML
-
-        // this is the imported HTML content of the templateElement (node includes a HTML tag and its content)
-        const importedNode = document.importNode(this.templateElement.content, true)
-        this.element = importedNode.firstElementChild as HTMLFormElement
-        this.element.id = 'user-input'  // apply the .user-input in the app.css
-
+        super('project-input', 'app', true, 'user-input')
         this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement  // Returns the first matching element that is a descendant of the form node
         this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement  // Returns the first matching element that is a descendant of the form node
         this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement  // Returns the first matching element that is a descendant of the form node
@@ -257,9 +262,15 @@ class ProjectInput {
 
         // sets up the necessary template + logic in the constructor by calling our methods, so its ready to go when an instance is created
         this.configure()
-        this.attach()
     }
 
+    configure() { // attach's an addEventListener to the <form></form> so we can submit
+        this.element.addEventListener('submit', this.submitHandler)  // when the form is submitted, execute the logic inside submitHandler() method  !!! (if you dont want to create an @AutoBind decorator... you could simply just use the bind method and bind 'this' context when entering the submitHandler like this -----> this.submitHandler.bind(this) ) !!!
+    }
+
+    // need to have cause its an abstract method, so even tho we dont do anything with it we still need it
+    renderContent(): void {
+    }
 
     private gatherUserInput(): [string, string, number] | void {  // get all userInput fields and return them so they can be used in the submitHandler (returns a Tuple)
         const enteredTitle = this.titleInputElement.value
@@ -319,14 +330,6 @@ class ProjectInput {
             this.clearInputs() // clear form on a successful submission
         }
 
-    }
-
-    private configure() { // attach's an addEventListener to the <form></form> so we can submit
-        this.element.addEventListener('submit', this.submitHandler)  // when the form is submitted, execute the logic inside submitHandler() method  !!! (if you dont want to create an @AutoBind decorator... you could simply just use the bind method and bind 'this' context when entering the submitHandler like this -----> this.submitHandler.bind(this) ) !!!
-    }
-
-    private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);  // !!!!!! INSERT THE FORM INTO THE <div id="app"></div> INTO THE HTML SO WE CAN DISPLAY IT !!!!!! //
     }
 }
 
